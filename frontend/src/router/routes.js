bbbe03b26333a62ef773b2169512ea46a212a64f
wonderjs <25072556+wonderjs@users.noreply.github.com>
2025-09-06 19:58:23 +0200

    add different layouts

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,30 +1,27 @@
 export const routes = [
   {
-    path: '',
+    path: '/',
     name: 'home',
     component: () => import('@/views/HomeView.vue'),
+    meta: { layout: 'DefaultLayout' },
   },
   {
     path: '/login',
     name: 'login',
     component: () => import('@/views/LoginView.vue'),
+    meta: { layout: 'SimpleLayout' },
   },
   {
     path: '/cart',
     name: 'cart',
     component: () => import('@/views/CartView.vue'),
+    meta: { layout: 'DefaultLayout' },
   },
   {
     path: '/user',
     name: 'user',
     component: () => import('@/views/UserView.vue'),
-    /*
-     * В шаблонах использована одна и та же страница,
-     * на которой меняется лишь часть контента
-     * в зависимости от выбранной вкладки.
-     *
-     * Поэтому здесь разумно использовать дочерние маршруты.
-     */
+    meta: { layout: 'DefaultLayout' },
     children: [
       {
         path: 'orders',
