d0b2e1607dbf909b2370870b4e704ee508160f7c
wonderjs <25072556+wonderjs@users.noreply.github.com>
2025-09-08 20:13:56 +0200

    add UserView

---
+++ frontend/src/views/UserView.vue
@@ -0,0 +1,33 @@
+<template>
+  <sidebar-layout>
+    <template #sidebar>
+      <router-link
+        class="layout__link"
+        :class="{ 'layout__link--active': routeName === 'orders' }"
+        :to="{ name: 'orders' }"
+      >
+        История заказов
+      </router-link>
+      <router-link
+        class="layout__link"
+        :class="{ 'layout__link--active': routeName === 'profile' }"
+        :to="{ name: 'profile' }"
+      >
+        Мои данные
+      </router-link>
+    </template>
+
+    <!--  Здесь будут располагаться компоненты дочерних маршрутов  -->
+    <router-view />
+  </sidebar-layout>
+</template>
+
+<script setup>
+import SidebarLayout from '@/layouts/SidebarLayout.vue';
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+
+const route = useRoute();
+
+const routeName = computed(() => route.name);
+</script>
