e1c989f5634a731a3aa88ad6419745d83d02633d
wonderjs <25072556+wonderjs@users.noreply.github.com>
2025-09-09 21:48:31 +0200

    add getters to stores

--- frontend/src/stores/cart.js
+++ frontend/src/stores/cart.js
@@ -1,4 +1,6 @@
 import { defineStore } from 'pinia';
+import { pizzaPrice } from '@/common/helpers/pizza-price';
+import { useDataStore } from '@/stores/data';
 
 export const useCartStore = defineStore('cart', {
   state: () => ({
@@ -12,6 +14,49 @@ export const useCartStore = defineStore('cart', {
     pizzas: [],
     misc: [],
   }),
-  getters: {},
+  getters: {
+    pizzasExtended: (state) => {
+      const data = useDataStore();
+
+      return state.pizzas.map((pizza) => {
+        const pizzaIngredientsIds = pizza.ingredients.map(
+          (i) => i.ingredientId
+        );
+
+        return {
+          name: pizza.name,
+          quantity: pizza.quantity,
+          dough: data.doughs.find((i) => i.id === pizza.doughId),
+          size: data.sizes.find((i) => i.id === pizza.sizeId),
+          sauce: data.sauces.find((i) => i.id === pizza.sauceId),
+          ingredients: data.ingredients.filter((i) =>
+            pizzaIngredientsIds.includes(i.id)
+          ),
+          price: pizzaPrice(pizza),
+        };
+      });
+    },
+    miscExtended: (state) => {
+      const data = useDataStore();
+
+      return data.misc.map((misc) => {
+        return {
+          ...misc,
+          quantity: state.misc.find((i) => i.miscId === misc.id)?.quantity ?? 0,
+        };
+      });
+    },
+    total: (state) => {
+      const pizzaPrices = state.pizzasExtended
+        .map((item) => item.quantity * item.price)
+        .reduce((acc, val) => acc + val, 0);
+
+      const miscPrices = state.miscExtended
+        .map((item) => item.quantity * item.price)
+        .reduce((acc, val) => acc + val, 0);
+
+      return pizzaPrices + miscPrices;
+    },
+  },
   actions: {},
 });
