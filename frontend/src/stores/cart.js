5e6113740d6b5a61b881ac4fbc14423ead21d509
wonderjs <25072556+wonderjs@users.noreply.github.com>
2025-09-10 20:21:22 +0200

    add actions to card store

--- frontend/src/stores/cart.js
+++ frontend/src/stores/cart.js
@@ -58,5 +58,70 @@ export const useCartStore = defineStore('cart', {
       return pizzaPrices + miscPrices;
     },
   },
-  actions: {},
+  actions: {
+    savePizza(pizza) {
+      const { index, ...pizzaData } = pizza;
+
+      if (index !== null) {
+        this.pizzas[index] = {
+          quantity: this.pizzas[index].quantity,
+          ...pizzaData,
+        };
+      } else {
+        this.pizzas.push({
+          quantity: 1,
+          ...pizzaData,
+        });
+      }
+    },
+    setPizzaQuantity(index, count) {
+      if (this.pizzas[index]) {
+        this.pizzas[index].quantity = count;
+      }
+    },
+    setMiscQuantity(miscId, count) {
+      const miscIdx = this.misc.findIndex((item) => item.miscId === miscId);
+
+      /*
+       * Добавляем ингредиент, если его нет, а количество больше 0
+       * Если ингредиента нет, а количество 0 или меньше, то ничего не делаем
+       */
+      if (miscIdx === -1 && count > 0) {
+        this.misc.push({
+          miscId,
+          quantity: 1,
+        });
+        return;
+      } else if (miscIdx === -1) {
+        return;
+      }
+
+      /* Удаляем ингредиент, если количество 0 */
+      if (count === 0) {
+        this.misc.splice(miscIdx, 1);
+        return;
+      }
+
+      this.misc[miscIdx].quantity = count;
+    },
+    setPhone(phone) {
+      this.phone = phone;
+    },
+    setAddress(address) {
+      const { street, building, flat, comment } = address;
+      this.address = { street, building, flat, comment };
+    },
+    setStreet(street) {
+      this.address.street = street;
+    },
+    setBuilding(building) {
+      this.address.building = building;
+    },
+    setFlat(flat) {
+      this.address.flat = flat;
+    },
+    setComment(comment) {
+      this.address.street = comment;
+    },
+  },
 });
